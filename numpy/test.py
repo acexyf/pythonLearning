661caabfbd35e36c15e71c59e3e5df482df798c6
xyf17469 <xyf17469@ly.com>
2018-06-05 09:24:44 +0800

    zancun

---
+++ numpy/test.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# m = np.array([np.arange(3), np.arange(3)])
+# print(m)
+# print(m.shape)
+
+
+# m1 = np.array([[1,2],[4,5]])
+# print(m1[0,1])
+# print(m1.shape)
+
+
+# m2 = np.arange(20)
+# print(m2[::-1])
+
+m3 = np.arange(24).reshape(2,3,4)
+print(m3)
+
+
+
+
+
+
+
+
+
+
+
